c257d0f3782d544c33d89364361c1351cc718a42
starling13 <starling13@mail.ru>
2020-10-26 17:46:14 +0300

    Add missing file

---
+++ include/sdlvt100print.hpp
@@ -0,0 +1,55 @@
+/*
+ * Libarduinoemulator is a simple library to emulate ArduinoIDE API on a Linux PC
+ * Copyright (C) 2016-2018 Andrey V. Skvortsov <starling13@mail.ru>
+ * Copyright (C) 2019,2020 Terminal-BASIC team
+ *     <https://bitbucket.org/%7Bf50d6fee-8627-4ce4-848d-829168eedae5%7D/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SDLVT100PRINT_HPP
+#define SDLVT100PRINT_HPP
+
+#include "vt100.hpp"
+#include "SDL.h"
+
+class SDL2vt100Print : public VT100::Print
+{
+public:
+	
+	SDL2vt100Print(SDL_Renderer&);
+	
+private:
+	
+	SDL_Renderer& m_renderer;
+
+// VT100::Print interface
+protected:
+	
+	void clear() override;
+
+	void writeChar(uint8_t) override;
+	
+	uint8_t getCursorX() override;
+
+	void setCursorX(uint8_t) override;
+	
+	void setCursor(uint8_t, uint8_t) override;
+	
+	void addAttribute(VT100::TextAttr) override;
+	
+	void resetAttributes() override;
+};
+
+#endif // SDLVT100PRINT_HPP
