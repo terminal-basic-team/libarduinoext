e3d778f89f74ed3a518a0fd5fb5311f0671dc093
starling13 <starling13@mail.ru>
2021-04-05 11:04:03 +0300

    Add buffered termiunal

---
+++ include/buffered_terminal.hpp
@@ -0,0 +1,65 @@
+/*
+ * ArduinoExt is a set of utility libraries for Arduino
+ * 
+ * Copyright (C) 2016-2018 Andrey V. Skvortsov <starling13@mail.ru>
+ * Copyright (C) 2019-2021 Terminal-BASIC team
+ *     <https://bitbucket.org/%7Bf50d6fee-8627-4ce4-848d-829168eedae5%7D/>
+ *
+ * This program is free software: is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+
+ * ArduinoExt library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Posixcpp library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BUFFERED_TERMINAL_HPP
+#define BUFFERED_TERMINAL_HPP
+
+#include "vt100.hpp"
+
+class BufferedTerminal : public VT100::Print
+{
+	EXT_NOTCOPYABLE(BufferedTerminal)
+
+public:
+	/**
+	 * @param rows
+	 * @param columns
+	 */
+	void begin(
+	    uint16_t,
+	    uint16_t);
+	
+	void update();
+
+protected:
+	
+	virtual void drawCursor(bool) = 0;
+	
+	// Current row and column
+	uint16_t m_row, m_column;
+	// Number of rows and columns
+	uint16_t m_rows, m_columns;
+	// Cursor lock object and blinking cursor state
+	bool m_lockCursor, m_cursorState;
+	// Cursor counter and period
+	uint8_t m_cursorCounter, m_cursorBlinkPeriod = 20;
+	// Current attribute
+	VT100::TextAttr m_attr;
+	
+// VT100::Print interface
+protected:
+	uint8_t getCursorX() override;
+
+	void setCursorX(uint8_t) override;
+};
+
+#endif // BUFFERED_TERMINAL_HPP
---
+++ src/buffered_terminal.cpp
@@ -0,0 +1,62 @@
+/*
+ * ArduinoExt is a set of utility libraries for Arduino
+ * 
+ * Copyright (C) 2016-2018 Andrey V. Skvortsov <starling13@mail.ru>
+ * Copyright (C) 2019-2021 Terminal-BASIC team
+ *     <https://bitbucket.org/%7Bf50d6fee-8627-4ce4-848d-829168eedae5%7D/>
+ *
+ * This program is free software: is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+
+ * ArduinoExt library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Posixcpp library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "buffered_terminal.hpp"
+
+void
+BufferedTerminal::begin(uint16_t rows, uint16_t columns)
+{
+	m_row = m_column = 0;
+	m_rows = rows, m_columns = columns;
+	m_attr = VT100::TextAttr::NO_ATTR;
+	m_lockCursor = false;
+	m_cursorState = true;
+}
+
+void
+BufferedTerminal::update()
+{
+	// If cursor is locked by the non-interrupt code - return
+	if (m_lockCursor)
+		return;
+
+	// Count down cursor blank interrupt counter
+	if (--m_cursorCounter == 0) {
+		m_cursorCounter = m_cursorBlinkPeriod;
+	} else
+		return;
+
+	m_cursorState = !m_cursorState;
+	drawCursor(m_cursorState);
+}
+
+uint8_t
+BufferedTerminal::getCursorX()
+{
+	return m_column;
+}
+
+void
+BufferedTerminal::setCursorX(uint8_t x)
+{
+	setCursor(x, m_row);
+}
